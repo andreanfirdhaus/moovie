fa19e04fb48fe2845038b2c6a399e9fc312091e1
andreanfirdhaus <andreanfirdhaus22@gmail.com>
2026-01-13 18:50:42 +0700

    unused

--- src/config/config.jsx
+++
@@ -1,3 +0,0 @@
-export const img_300 = 'https://image.tmdb.org/t/p/w300';
-export const original = 'https://image.tmdb.org/t/p/original';
-export const img_404 = 'https://www.movienewz.com/img/films/poster-holder.jpg';
