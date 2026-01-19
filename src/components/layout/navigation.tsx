ad8181bda7cf175005daa11753cdbccb33ad2471
andreanfirdhaus <andreanfirdhaus22@gmail.com>
2026-01-19 13:01:52 +0700

    fix: path logo svg

--- src/components/layout/navigation.tsx
+++ src/components/layout/navigation.tsx
@@ -1,6 +1,6 @@
 import { useState } from 'react';
 import { Link, NavLink } from 'react-router-dom';
-import Logo from '../../../assets/logo.svg';
+import Logo from '@/assets/logo.svg';
 import { Search } from 'lucide-react';
 import { menu } from '@/constants/menu';
 
