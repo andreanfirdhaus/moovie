09302c26fc52af1c56f3d51fafe559fb1249bb27
andreanfirdhaus <andreanfirdhaus22@gmail.com>
2026-01-19 12:15:09 +0700

    feat(test): not found page

---
+++ src/not-found.tsx
@@ -0,0 +1,9 @@
+export default function NotFound() {
+    return (
+        <main className='h-screen z-20 absolute top-0 left-0 w-full bg-black'>
+            <div className='flex items-center justify-center h-full'>
+                <h1 className='text-6xl font-bold text-zinc-400'>404 - Page Not Found</h1>
+            </div>
+        </main>
+    );
+}
