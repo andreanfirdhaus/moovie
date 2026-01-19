2cf43b758334b6d5a3d13d42bc1b398e54c7edae
andreanfirdhaus <andreanfirdhaus22@gmail.com>
2026-01-19 12:41:53 +0700

    test: discovery page

---
+++ src/pages/discover.tsx
@@ -0,0 +1,7 @@
+export default function Discover() {
+    return (
+        <main>
+            <h1 className='h-screen flex justify-center items-center text-zinc-300'>discover pages</h1>
+        </main>
+    );
+}
