9be28e016b57817523acf92c0f1f3f66c8815f4f
andreanfirdhaus <andreanfirdhaus22@gmail.com>
2026-01-19 12:46:20 +0700

    feat(routes): define main routes

---
+++ src/routes.tsx
@@ -0,0 +1,30 @@
+import Layout from '@/layout';
+import Home from '@/pages/home';
+import Discover from '@/pages/discover';
+import DetailPage from './pages/[detail]';
+import NotFound from './not-found';
+
+export const routes = [
+    {
+        path: '/',
+        element: <Layout />,
+        children: [
+            {
+                path: '/',
+                element: <Home />,
+            },
+            {
+                path: '/discover',
+                element: <Discover />,
+            },
+            {
+                path: '/:type/detail/:id',
+                element: <DetailPage />,
+            },
+            {
+                path: '*',
+                element: <NotFound />,
+            },
+        ],
+    },
+];
