78cb54c8a7f7554ad33e6ecc0c1d8f357afb03f3
andreanfirdhaus <andreanfirdhaus22@gmail.com>
2026-01-21 07:49:07 +0700

    fix: path of directory files

--- src/utils/tmdb-image.ts
+++ src/utils/tmdb-image.ts
@@ -1,4 +1,4 @@
-import FallbackPoster from '@/assets/profile-placeholder.png';
+import FallbackPoster from '../../public/assets/profile-placeholder.png';
 
 export const TMDB_IMG_300 = 'https://image.tmdb.org/t/p/w300';
 export const TMDB_IMG_ORIGINAL = 'https://image.tmdb.org/t/p/original';
