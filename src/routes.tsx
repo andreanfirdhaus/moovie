fe08e05dd67f420a388149387a84abc80de26875
andreanfirdhaus <andreanfirdhaus22@gmail.com>
2026-01-22 07:20:27 +0700

    set home as default index route

--- src/routes.tsx
+++ src/routes.tsx
@@ -10,7 +10,7 @@ export const routes = [
         element: <Layout />,
         children: [
             {
-                path: '/',
+                index: true,
                 element: <Home />,
             },
             {
