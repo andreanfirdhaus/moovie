fb2af422d9901d5ec7002536a599f036f836c87d
andreanfirdhaus <andreanfirdhaus22@gmail.com>
2026-01-28 14:52:24 +0700

    style: upcoming content

--- src/pages/discover.tsx
+++ src/pages/discover.tsx
@@ -1,7 +1,7 @@
 export default function Discover() {
     return (
         <main>
-            <h1 className='h-screen flex justify-center items-center text-zinc-300'>discover pages</h1>
+            <h1 className='h-screen flex justify-center items-center text-zinc-300'>UPCOMING..</h1>
         </main>
     );
 }
