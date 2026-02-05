6e86de5eefc6ca9030833d02c7a5b578e797cb4b
andreanfirdhaus <andreanfirdhaus22@gmail.com>
2026-02-05 22:35:24 +0700

    fix: not-found page

--- src/not-found.tsx
+++ src/not-found.tsx
@@ -1,8 +1,23 @@
+import { Link } from 'react-router-dom';
+
 export default function NotFound() {
     return (
-        <main className='h-screen z-20 absolute top-0 left-0 w-full bg-black'>
-            <div className='flex items-center justify-center h-full'>
-                <h1 className='text-6xl font-bold text-zinc-400'>404 - Page Not Found</h1>
+        <main className='fixed inset-0 z-20 flex items-center justify-center bg-black'>
+            <div className='flex flex-col items-center justify-center gap-4 px-4 text-center sm:gap-6'>
+                <img
+                    className='h-8 w-auto sm:h-10 md:h-16'
+                    src='/assets/not-found.png'
+                    alt='Page not found illustration'
+                    draggable='false'
+                    loading='eager'
+                />
+
+                <p className='sm:text-lg text-zinc-500 font-medium'>
+                    The page you&apos;re looking for doesn&apos;t exist. Back to{' '}
+                    <Link to='/' className='hover:text-zinc-300 transition-colors'>
+                        home.
+                    </Link>
+                </p>
             </div>
         </main>
     );
