40feada8f51875fe6016e7ca98b4597944bf3db7
andreanfirdhaus <andreanfirdhaus22@gmail.com>
2026-02-07 18:16:30 +0700

    unused: restructure project directory

--- src/pages/discover.tsx
+++
@@ -1,7 +0,0 @@
-export default function Discover() {
-    return (
-        <main>
-            <h1 className='h-screen flex justify-center items-center text-zinc-300'>UPCOMING..</h1>
-        </main>
-    );
-}
