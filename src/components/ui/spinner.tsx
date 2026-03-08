02833d91cb9618f51c6d0c9f01994340b00b74f4
andreanfirdhaus <andreanfirdhaus22@gmail.com>
2026-03-08 07:09:10 +0700

    style: improve class utilits

--- src/components/ui/spinner.tsx
+++ src/components/ui/spinner.tsx
@@ -2,7 +2,7 @@ import { Loader2 } from 'lucide-react';
 
 export default function Loading() {
     return (
-        <div className='flex items-center justify-center h-dvh absolute inset-0 z-50 bg-black'>
+        <div className='flex items-center justify-center h-dvh'>
             <Loader2 className='animate-spin text-brand mx-auto mb-2' size={48} />
         </div>
     );
