98f12a74b9513d78a62415ad4fd786d22af84bd1
andreanfirdhaus <andreanfirdhaus22@gmail.com>
2026-03-21 22:18:34 +0700

    feat(app): configure root entry point

--- src/routes.tsx
+++ src/routes.tsx
@@ -1,15 +1,12 @@
+import { lazy } from 'react';
 import Layout from '@/layout';
-import Home from '@/pages/home';
-import Discover from '@/pages/discover';
-import DetailPage from '@/pages/detail';
-import Streaming from '@/pages/streaming';
-import WatchPage from '@/pages/watch';
 import NotFound from './not-found';
-import MoviePopular from '@/pages/movie/popular';
-import MovieUpcoming from '@/pages/movie/upcoming';
-import MovieTopRated from '@/pages/movie/toprated';
-import TvPopular from '@/pages/tv/popular';
-import TvTopRated from '@/pages/tv/toprated';
+
+const Home = lazy(() => import('@/features/home'));
+const DetailPage = lazy(() => import('@/features/detail'));
+const WatchPage = lazy(() => import('@/features/watch'));
+const Search = lazy(() => import('@/features/search'));
+const DiscoverPage = lazy(() => import('./features/discover/components/discover-page'));
 
 export const routes = [
     {
@@ -17,15 +14,22 @@ export const routes = [
         element: <Layout />,
         children: [
             { index: true, element: <Home /> },
-            // browse routes with explicit paths
-            { path: '/movie/popular', element: <MoviePopular /> },
-            { path: '/movie/upcoming', element: <MovieUpcoming /> },
-            { path: '/movie/toprated', element: <MovieTopRated /> },
-            { path: '/tv/popular', element: <TvPopular /> },
-            { path: '/tv/toprated', element: <TvTopRated /> },
+            {
+                path: '/movie/popular',
+                element: <DiscoverPage key='movie-popular' mediaType='movie' category='popular' />,
+            },
+            {
+                path: '/movie/upcoming',
+                element: <DiscoverPage key='movie-upcoming' mediaType='movie' category='upcoming' />,
+            },
+            {
+                path: '/movie/toprated',
+                element: <DiscoverPage key='movie-toprated' mediaType='movie' category='toprated' />,
+            },
+            { path: '/tv/popular', element: <DiscoverPage key='tv-popular' mediaType='tv' category='popular' /> },
+            { path: '/tv/toprated', element: <DiscoverPage key='tv-toprated' mediaType='tv' category='toprated' /> },
 
-            { path: '/discover', element: <Discover /> }, // search query
-            { path: '/streaming', element: <Streaming /> },
+            { path: '/search', element: <Search /> },
             { path: '/:type/:id', element: <DetailPage /> },
             { path: '/:type/watch/:id', element: <WatchPage /> },
             { path: '*', element: <NotFound /> },
