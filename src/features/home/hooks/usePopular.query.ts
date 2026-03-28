c35aeaa5c54901830edd7fedcbe091af20673553
andreanfirdhaus <andreanfirdhaus22@gmail.com>
2026-03-29 06:37:45 +0700

    refactor: improve hooks params

--- src/features/home/hooks/usePopular.query.ts
+++ src/features/home/hooks/usePopular.query.ts
@@ -6,18 +6,34 @@ import { TMDB_PARAMS } from '@/config/params';
 
 export type PopularFilter = 'streaming' | 'on-tv' | 'in-theaters';
 
-const params = {
+const streamingParams = {
     with_watch_monetization_types: 'flatrate',
     watch_region: TMDB_PARAMS.region,
-    with_watch_providers: 8, // netflix
-    'first_air_date.gte': '2016-01-01',
+    with_watch_providers: '8|337|119', // netflix|disney+|prime
+    sort_by: 'popularity.desc',
+    'vote_average.gte': 6,
+    'vote_count.gte': 100,
+    'primary_release_date.gte': '2018-01-01',
+};
+
+const onTVParams = {
+    with_watch_monetization_types: 'free|ads',
+    watch_region: TMDB_PARAMS.region,
+    sort_by: 'popularity.desc',
+    without_genres: '16',
+    'vote_average.gte': 6,
+    'vote_count.gte': 70,
+    'first_air_date.gte': '2017-01-01',
 };
 
 export const useStreaming = () =>
     useQuery({
         queryKey: queryKeys.popular.streaming(),
         queryFn: async () => {
-            const [movieRes, tvRes] = await Promise.all([getDiscoverMovies(1, params), getDiscoverSeries(1, params)]);
+            const [movieRes, tvRes] = await Promise.all([
+                getDiscoverMovies(1, streamingParams),
+                getDiscoverSeries(1, streamingParams),
+            ]);
 
             const movies = movieRes.data.results.slice(0, 10);
             const tv = tvRes.data.results.slice(0, 10);
@@ -36,16 +52,7 @@ export const useOnTVSeries = () =>
     useQuery({
         queryKey: queryKeys.popular.onTV(),
         queryFn: async () => {
-            const res = await getDiscoverSeries(1, {
-                with_watch_monetization_types: 'flatrate',
-                watch_region: TMDB_PARAMS.region,
-                sort_by: 'popularity.desc',
-                without_genres: '16', // animation
-                'vote_average.gte': 6,
-                'vote_count.gte': 50,
-                'first_air_date.gte': '2017-01-01',
-                with_watch_providers: 8, // netflix
-            });
+            const res = await getDiscoverSeries(1, onTVParams);
             return res.data.results;
         },
         staleTime: 1000 * 60 * 10,
