34cfa466ca5da214207ba42f911238073a9e2c03
andreanfirdhaus <andreanfrdhaus22@gmail.com>
2026-09-15 00:05:02 +0700

    feat: watch provider lists

---
+++ src/constants/watch-providers.ts
@@ -0,0 +1,38 @@
+export interface WatchProvider {
+    id: number;
+    name: string;
+    logo_path: string;
+}
+
+export const WATCH_PROVIDERS: WatchProvider[] = [
+    {
+        id: 350,
+        name: 'Apple TV+',
+        logo_path: '/mcbz1LgtErU9p4UdbZ0rG6RTWHX.jpg',
+    },
+    {
+        id: 283,
+        name: 'Crunchyroll',
+        logo_path: '/fzN5Jok5Ig1eJ7gyNGoMhnLSCfh.jpg',
+    },
+    {
+        id: 337,
+        name: 'Disney+',
+        logo_path: '/97yvRBw1GzX7fXprcF80er19ot.jpg',
+    },
+    {
+        id: 15,
+        name: 'Hulu',
+        logo_path: '/bxBlRPEPpMVDc4jMhSrTf2339DW.jpg',
+    },
+    {
+        id: 8,
+        name: 'Netflix',
+        logo_path: '/pbpMk2JmcoNnQwx5JGpXngfoWtp.jpg',
+    },
+    {
+        id: 119,
+        name: 'Prime Video',
+        logo_path: '/emthp39XA2YScoYL1p0sdbAH2WA.jpg',
+    },
+];
