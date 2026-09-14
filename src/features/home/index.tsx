d6bc2bffad848542dd86e7c82bfd7e3e732ba42b
andreanfirdhaus <andreanfrdhaus22@gmail.com>
2026-09-15 02:34:30 +0700

    feat(home): add Japanese anime collection section

--- src/features/home/index.tsx
+++ src/features/home/index.tsx
@@ -1,5 +1,6 @@
 import { MediaHero } from '@/components/media/hero';
 import { PopularSection } from './components/popular-section';
+import MediaCollection from '@/components/media/media-collection';
 
 export default function Home() {
     return (
@@ -9,6 +10,8 @@ export default function Home() {
 
             <div className='mt-8 sm:mt-10 space-y-6'>
                 <PopularSection />
+
+                <MediaCollection title='Anime' mediaType='tv' genreIds={[16, 10759]} watchProvider={337} country='JP' />
             </div>
         </main>
     );
